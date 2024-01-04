791471b67713668f8a6360142c209a78e19d8040
kokomong2 <ko9603@naver.com>
2024-01-04 16:05:57 +0900

    fix: cast char 를 int로 수정

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -12,7 +12,7 @@ int	main(int argc, char *argv[])
 		for (int i = 1; i < argc; i++)
 		{
 			std::string str = argv[i];
-			for (int j = 0; j < static_cast<char>(str.length()); j++)
+			for (int j = 0; j < static_cast<int>(str.length()); j++)
 			{
 				std::cout << static_cast<char>(std::toupper(str[j]));
 			}
