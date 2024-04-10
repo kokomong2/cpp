28c4b5e9020ba19f98188052de3eee7952e8a12e
kokomong2 <ko9603@naver.com>
2024-04-10 13:50:08 +0900

    refactor: cpp07 함수 포인터를 템플릿으로 처리 하도록 수정

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,8 +3,8 @@
 
 #include <iostream>
 
-template<typename T>
-void iter (T *array, size_t length, void (*fn)(T&))
+template<typename T, typename F>
+void iter (T *array, size_t length, F fn)
 {
 	for (size_t i = 0; i < length; i++)
 	{
@@ -13,15 +13,9 @@ void iter (T *array, size_t length, void (*fn)(T&))
 }
 
 template<typename T>
-void print(T &str)
+void printIter(T &str)
 {
 	std::cout << str << " ";
-}
-
-template<typename T>
-void sqrt(T &num)
-{
-	num = num * num;
 }
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -3,11 +3,7 @@
 int main(int, char**)
 {
 	int tab[] = {0, 1, 2, 3, 4};
-	iter(tab, 5, print);
-	std::cout << std::endl;
-	iter(tab, 5, sqrt);
-	std::cout << std::endl;
-	iter(tab, 5, print);
-	std::cout << std::endl;
+	std::cout << "Original array: " << std::endl;
+	iter(tab, 5, printIter<int>);
 	return (0);
 }
